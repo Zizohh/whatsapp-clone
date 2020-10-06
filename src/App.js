803526c98ecb276aa353836845ed845aca2d4a38
Zizohh <jeremiahhussaini2892@gmail.com>
2020-10-06 17:42:09 +0100

    Initial frontend commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Pusher from "pusher-js";
+import "./App.css";
+import Sidebar from "./Sidebar";
+import Chat from "./Chat";
+import axios from "./axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [messages, setMessages] = useState([]);
+
+	useEffect(() => {
+		axios.get("/messages/sync").then((response) => {
+			console.log(response.data);
+			setMessages(response.data);
+		});
+	}, []);
+
+	useEffect(() => {
+		const pusher = new Pusher("c6079bad04ed8265a2e5", {
+			cluster: "eu",
+		});
+
+		const channel = pusher.subscribe("messages");
+		channel.bind("inserted", (newMessage) => {
+			JSON.stringify(newMessage);
+			setMessages([...messages, newMessage]);
+		});
+
+		return () => {
+			channel.unbind_all();
+			channel.unsubscribe();
+		};
+	}, [messages]);
+
+	console.log(messages);
+
+	return (
+		<div className="app">
+			<div className="app_body">
+				<Sidebar />
+				<Chat messages={messages} />
+			</div>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/Sidebar.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import "./Sidebar.css";
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import { Avatar, IconButton } from "@material-ui/core";
+import ChatIcon from "@material-ui/icons/Chat";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import { SearchOutlined } from '@material-ui/icons';
+import SidebarChat from './SidebarChat';
+
+function Sidebar() {
+    return (
+        <div className='sidebar'>
+             <div className= "sidebar_header">
+                 <Avatar src="https://avatars1.githubusercontent.com/u/44261019?s=460&u=d68284436e44c495ea1ea819bcf5430fc0361d2c&v=4"/>
+                 <div className="sidebar_headerRight">
+                    <IconButton>
+                        <DonutLargeIcon />
+                    </IconButton> 
+                    <IconButton>
+                        <ChatIcon />
+                    </IconButton> 
+                    <IconButton>
+                        <MoreVertIcon />
+                    </IconButton>                    
+                 </div>
+             </div>
+            
+            <div className="sidebar_search">
+                <div className="sidebar_searchContainer">
+                     <SearchOutlined />
+                     <input placeholder="Search or start new chat" type="text" />
+                </div>
+            </div>
+
+            <div className="sidebar_chats">
+                 <SidebarChat />
+                 <SidebarChat />
+                 <SidebarChat />   
+            </div>
+        </div>
+    );
+}
+
+
+export default Sidebar;
